b31af0ba67724da9a6a80349c97cf6d7cef4c98a
werliton <werlitoncarlos@gmail.com>
2025-05-23 16:15:27 -0300

    feat: :sparkles: define initial mobile first layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,101 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-nunito-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+    <div className="bg-background m-auto grid min-h-screen w-[390px] min-[390px]:min-w-[390px] sm:w-full">
+      <header className="bg-primary flex h-32 min-w-full items-center gap-6 p-4 sm:grid sm:grid-cols-3">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/icons/brand.svg"
+          alt="Endereço"
+          width={32}
+          height={32}
           priority
         />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-nunito-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-nunito-mono)] font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <div className="flex h-[2.5rem] grow items-center gap-2.5 sm:col-span-1">
+          <Image
+            className="dark:invert"
+            src="/icons/marker.svg"
+            alt="Endereço"
+            width={24}
+            height={24}
+            priority
+          />
+          <div className="flex flex-col items-start gap-0.5">
+            <p className="text-subtitle text-sm font-bold">entregando em</p>
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="flex items-center gap-1 self-stretch">
+              <p className="text-primary-foreground text-base font-bold">
+                Rua Mandaguari, 198
+              </p>{" "}
+              <Image
+                className="dark:invert"
+                src="/icons/right-arrow.svg"
+                alt="arrow"
+                width={24}
+                height={24}
+                priority
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <Image
+          className="sm:justify-self-end dark:invert"
+          src="/icons/user.svg"
+          alt="User"
+          width={24}
+          height={24}
+          priority
+        />
+      </header>
+      <aside></aside>
+      <main></main>
+      <footer className="bg-muted text-footer flex h-[119px] flex-col items-center justify-center gap-0.5 px-4 py-6">
+        <p className="text-sm font-bold">Feito com amor em Maringá-PR</p>
+        <p className="text-base font-bold">
+          aiqfome.com © 2007-2023 aiqfome LTDA . CNPJ: 09.186.786/0001-58
+        </p>
       </footer>
     </div>
   );
